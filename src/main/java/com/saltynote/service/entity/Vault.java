d14eaaedb8225cc9b5eb754633e8bf1a2b17d808
hzhou <hzhou.me@gmail.com>
2020-12-12 23:07:07 -0800

    add vault table and its resource

---
+++ src/main/java/com/saltynote/service/entity/Vault.java
@@ -0,0 +1,41 @@
+package com.saltynote.service.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Entity
+@Table(name = "vault")
+@Data
+@Accessors(chain = true)
+public class Vault implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "user_id", nullable = false)
+    private Integer userId;
+
+    @Column(name = "secret", nullable = false)
+    private String secret;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "created_time", nullable = false)
+    private LocalDateTime createdTime;
+
+}
