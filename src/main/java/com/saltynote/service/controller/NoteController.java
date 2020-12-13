d39a7291be56421157f2fedf8b82c48a81afc8b4
Hao Zhou <hzhou.me@gmail.com>
2020-12-12 17:49:23 -0800

    add update and delete endpoints for note

--- src/main/java/com/saltynote/service/controller/NoteController.java
+++ src/main/java/com/saltynote/service/controller/NoteController.java
@@ -8,14 +8,18 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.saltynote.service.domain.transfer.JwtUser;
+import com.saltynote.service.domain.transfer.ServiceResponse;
 import com.saltynote.service.entity.Note;
+import com.saltynote.service.exception.WebClientRuntimeException;
 import com.saltynote.service.repository.NoteRepository;
 import lombok.extern.slf4j.Slf4j;
 
@@ -29,9 +33,30 @@ public class NoteController {
   }
 
   @GetMapping("/note/{id}")
-  public ResponseEntity<Note> getNote(@PathVariable("id") Integer id) {
+  public ResponseEntity<Note> getNote(@PathVariable("id") Integer id, Authentication auth) {
     Optional<Note> note = noteRepository.findById(id);
+    checkNoteOwner(note, auth);
     return note.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @PutMapping("/note/{id}")
+  public ResponseEntity<Note> updateNote(
+      @PathVariable("id") Integer id, @RequestBody Note note, Authentication auth) {
+    Optional<Note> queryNote = noteRepository.findById(id);
+    checkNoteOwner(queryNote, auth);
+    Note noteTobeUpdate =
+        queryNote.get().setNote(note.getNote()).setHighlightColor(note.getHighlightColor());
+    noteTobeUpdate = noteRepository.save(noteTobeUpdate);
+    return ResponseEntity.ok(noteTobeUpdate);
+  }
+
+  @DeleteMapping("/note/{id}")
+  public ResponseEntity<ServiceResponse> deleteNote(
+      @PathVariable("id") Integer id, Authentication auth) {
+    Optional<Note> note = noteRepository.findById(id);
+    checkNoteOwner(note, auth);
+    noteRepository.deleteById(id);
+    return ResponseEntity.ok(ServiceResponse.ok("Delete Successfully!"));
   }
 
   @GetMapping("/notes")
@@ -47,8 +72,16 @@ public class NoteController {
     note = noteRepository.save(note);
     if (note.getId() > 0) {
       return ResponseEntity.ok(note);
-    } else {
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
+    throw new RuntimeException("Failed to save note into database: " + note);
+  }
+
+  private void checkNoteOwner(Optional<Note> note, Authentication auth) {
+    JwtUser user = (JwtUser) auth.getPrincipal();
+    if (note.isPresent() && user.getId().equals(note.get().getUserId())) {
+      return;
+    }
+    throw new WebClientRuntimeException(
+        HttpStatus.FORBIDDEN, "Permission Error: You are not the owner of the note.");
   }
 }
