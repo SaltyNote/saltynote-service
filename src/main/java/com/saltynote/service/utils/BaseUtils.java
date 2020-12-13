2510ca6d63055ad70b438f4ca192763160e01d2d
hzhou <hzhou.me@gmail.com>
2020-12-12 23:07:46 -0800

    add base utils class

---
+++ src/main/java/com/saltynote/service/utils/BaseUtils.java
@@ -0,0 +1,20 @@
+package com.saltynote.service.utils;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.util.StringUtils;
+
+public class BaseUtils {
+  private static String baseUrl = "https://saltynote.com";
+
+  // This is used for test or dev usage, do not call it in prod.
+  public static void setBaseUrl(String _baseUrl) {
+    if (StringUtils.startsWithIgnoreCase(_baseUrl, "http")) {
+      baseUrl = _baseUrl;
+    }
+  }
+
+  public static String getConfirmationUrl(@NotNull String secret) {
+    return baseUrl + "/confirmation/" + secret;
+  }
+}
