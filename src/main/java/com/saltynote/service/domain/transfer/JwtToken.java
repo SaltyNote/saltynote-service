3e409972f967372af96d398ebbd13b9399f93ec3
Hao Zhou <hzhou.me@gmail.com>
2020-12-12 21:10:02 -0800

    fix jackson de-serialization error

--- src/main/java/com/saltynote/service/domain/transfer/JwtToken.java
+++ src/main/java/com/saltynote/service/domain/transfer/JwtToken.java
@@ -6,11 +6,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class JwtToken {
   @JsonProperty("access_token")
