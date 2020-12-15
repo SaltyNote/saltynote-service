918c9610c48d5bf235f89e5571b71dbd8cfd4e23
hzhou <hzhou.me@gmail.com>
2020-12-14 21:15:11 -0800

    enable controller calling log

---
+++ src/main/java/com/saltynote/service/aop/ControllerMonitor.java
@@ -0,0 +1,30 @@
+package com.saltynote.service.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+public class ControllerMonitor {
+
+  @Pointcut("execution(* com.saltynote.service.controller.*Controller.*(..))")
+  public void monitor() {
+    // an utility for aop
+  }
+
+  @Around("monitor()")
+  public Object logServiceAccess(ProceedingJoinPoint pjp) throws Throwable {
+    log.info("processing: " + pjp);
+    long start = System.currentTimeMillis();
+    Object output = pjp.proceed();
+    long elapsedTime = System.currentTimeMillis() - start;
+    log.info(pjp.getSignature() + " execution time: " + elapsedTime + " milliseconds.");
+    return output;
+  }
+}
