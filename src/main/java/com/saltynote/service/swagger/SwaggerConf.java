7b51feb3393d2ac1cb0c8d590b826e913196f2c3
Hao Zhou <hzhou.me@gmail.com>
2020-12-20 21:56:52 -0800

    change swagger annotation, and test can pass

--- src/main/java/com/saltynote/service/swagger/SwaggerConf.java
+++ src/main/java/com/saltynote/service/swagger/SwaggerConf.java
@@ -2,15 +2,15 @@ package com.saltynote.service.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableWebMvc
+@EnableSwagger2
 public class SwaggerConf {
   @Bean
   public Docket api() {
