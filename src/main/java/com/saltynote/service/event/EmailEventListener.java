0fce260d622b241010238bce377fadf48185fdc5
Hao Zhou <hzhou.me@gmail.com>
2020-12-28 23:04:09 -0800

    log email payload if smtp configuration is not available #32

--- src/main/java/com/saltynote/service/event/EmailEventListener.java
+++ src/main/java/com/saltynote/service/event/EmailEventListener.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 
 import javax.mail.MessagingException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import com.saltynote.service.domain.EmailPayload;
 import com.saltynote.service.domain.VaultType;
 import com.saltynote.service.entity.Vault;
 import com.saltynote.service.service.EmailService;
@@ -21,6 +24,9 @@ public class EmailEventListener {
   private final EmailService emailService;
   private final VaultService vaultService;
 
+  @Value("${spring.mail.username}")
+  private String mailUser;
+
   public EmailEventListener(EmailService emailService, VaultService vaultService) {
     this.emailService = emailService;
     this.vaultService = vaultService;
@@ -29,15 +35,21 @@ public class EmailEventListener {
   @EventListener
   public void handleEvent(EmailEvent event)
       throws MessagingException, IOException, TemplateException {
-    log.info(event.getType() + " - " + event.getUser());
+    log.info("Event is received for {}", event.getType());
     Vault vault = vaultService.create(event.getUser().getId(), VaultType.NEW_ACCOUNT);
-    emailService.sendAsHtml(
-        event.getUser().getEmail(),
-        event.getType().getSubject(),
+    EmailPayload payload =
         event
             .getType()
             .loadUser(event.getUser())
             .loadLinkInfo(vaultService.encode(vault))
-            .getPayload());
+            .getPayload();
+
+    if (StringUtils.hasText(mailUser)) {
+      emailService.sendAsHtml(event.getUser().getEmail(), event.getType().getSubject(), payload);
+    } else {
+      log.info("============================================");
+      log.info("Email Payload Info = {}", payload);
+      log.info("============================================");
+    }
   }
 }
