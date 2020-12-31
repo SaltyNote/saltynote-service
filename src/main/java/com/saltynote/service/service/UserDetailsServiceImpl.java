058a86b33698f02e913d2bae3fcddb232e7e3279
Hao Zhou <hzhou.me@gmail.com>
2020-12-31 00:02:04 -0800

    replace userRepository with userService

--- src/main/java/com/saltynote/service/service/UserDetailsServiceImpl.java
+++ src/main/java/com/saltynote/service/service/UserDetailsServiceImpl.java
@@ -1,33 +1,31 @@
 package com.saltynote.service.service;
 
+import java.sql.Timestamp;
+
+import javax.annotation.Resource;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.saltynote.service.domain.LoginUser;
 import com.saltynote.service.entity.SiteUser;
-import com.saltynote.service.repository.UserRepository;
-
-import java.sql.Timestamp;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-  private final UserRepository userRepository;
 
-  public UserDetailsServiceImpl(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
+  @Resource private UserService userService;
 
   @Override
   public LoginUser loadUserByUsername(String username) throws UsernameNotFoundException {
-    SiteUser user = userRepository.findByUsername(username);
+    SiteUser user = userService.getRepository().findByUsername(username);
     if (user == null) {
       throw new UsernameNotFoundException(username);
     }
 
     // issue #39 : update last_login_time when user login
     user.setLastLoginTime(new Timestamp(System.currentTimeMillis()));
-    userRepository.save(user);
+    userService.getRepository().save(user);
 
     return new LoginUser(user);
   }
