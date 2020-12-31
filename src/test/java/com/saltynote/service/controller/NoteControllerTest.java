98ba7da5ad43af1c86942555e4b13972d0cf7c4a
Hao Zhou <hzhou.me@gmail.com>
2020-12-31 00:41:44 -0800

    clean unused import

--- src/test/java/com/saltynote/service/controller/NoteControllerTest.java
+++ src/test/java/com/saltynote/service/controller/NoteControllerTest.java
@@ -47,7 +47,6 @@ import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTest
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
