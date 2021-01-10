deccc3a617c0ca663c1d6abf7ed1c1fabc4bf2f1
Hao Zhou <hzhou.me@gmail.com>
2021-01-09 22:56:21 -0800

    add UserNewRequest pojo for user signup

---
+++ src/main/java/com/saltynote/service/domain/transfer/UserNewRequest.java
@@ -0,0 +1,14 @@
+package com.saltynote.service.domain.transfer;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+public class UserNewRequest extends UserCredential {
+  @NotBlank private String token;
+}
