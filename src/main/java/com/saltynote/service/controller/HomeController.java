9b0aabe5c813c5092e7f7160d6803509937a8f1d
Hao Zhou <hzhou.me@saltyee.com>
2023-01-11 07:43:57 -0800

    upgrade dependencies and remove springfox document

--- src/main/java/com/saltynote/service/controller/HomeController.java
+++ src/main/java/com/saltynote/service/controller/HomeController.java
@@ -1,22 +1,17 @@
 package com.saltynote.service.controller;
 
+import com.saltynote.service.domain.transfer.ServiceResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.saltynote.service.domain.transfer.ServiceResponse;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
 @RestController
-@Api(value = "Home Endpoint", description = "Home endpoint for saltynote service")
 public class HomeController {
 
     @Value("${app.welcome.message}")
     private String welcomeMessage;
 
-    @ApiOperation("Display a greeting message")
     @GetMapping("/")
     public ResponseEntity<ServiceResponse> home() {
         return ResponseEntity.ok(ServiceResponse.ok(welcomeMessage));
--- src/main/java/com/saltynote/service/controller/NoteController.java
+++ src/main/java/com/saltynote/service/controller/NoteController.java
@@ -1,11 +1,14 @@
 package com.saltynote.service.controller;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
-
+import com.google.common.base.Splitter;
+import com.saltynote.service.domain.transfer.JwtUser;
+import com.saltynote.service.domain.transfer.NoteQuery;
+import com.saltynote.service.domain.transfer.ServiceResponse;
+import com.saltynote.service.entity.Note;
+import com.saltynote.service.exception.WebAppRuntimeException;
+import com.saltynote.service.service.NoteService;
+import com.saltynote.service.utils.BaseUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,24 +23,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.common.base.Splitter;
-import com.saltynote.service.domain.transfer.JwtUser;
-import com.saltynote.service.domain.transfer.NoteQuery;
-import com.saltynote.service.domain.transfer.ServiceResponse;
-import com.saltynote.service.entity.Note;
-import com.saltynote.service.exception.WebAppRuntimeException;
-import com.saltynote.service.service.NoteService;
-import com.saltynote.service.utils.BaseUtils;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
-@Api(
-        value = "Note Endpoint",
-        description =
-                "Everything about Note operation, all these operations require valid access token")
 public class NoteController {
     private final NoteService noteService;
 
@@ -46,8 +38,6 @@ public class NoteController {
     }
 
     @GetMapping("/note/{id}")
-    @ApiOperation(
-            "Get a single note by ID. You should be the owner of this note, otherwise, request is forbidden.")
     public ResponseEntity<Note> getNoteById(@PathVariable("id") String id, Authentication auth) {
         Optional<Note> note = noteService.getRepository().findById(id);
         checkNoteOwner(note, auth);
@@ -57,8 +47,6 @@ public class NoteController {
     @RequestMapping(
             value = "/note/{id}",
             method = {RequestMethod.POST, RequestMethod.PUT})
-    @ApiOperation(
-            "Update a note by ID, either with PUT or POST method. You should be the owner of this note, otherwise, request is forbidden.")
     public ResponseEntity<Note> updateNoteById(
             @PathVariable("id") String id, @RequestBody Note note, Authentication auth) {
         Optional<Note> queryNote = noteService.getRepository().findById(id);
@@ -80,8 +68,6 @@ public class NoteController {
     }
 
     @DeleteMapping("/note/{id}")
-    @ApiOperation(
-            "Delete a note by ID. You should be the owner of this note, otherwise, request is forbidden.")
     public ResponseEntity<ServiceResponse> deleteNoteById(
             @PathVariable("id") String id, Authentication auth) {
         Optional<Note> note = noteService.getRepository().findById(id);
@@ -93,8 +79,6 @@ public class NoteController {
     // TODO: this POST is required for chrome extension, as I find the PUT or DELETE requests will be
     // blocked by Chrome. Further investigation is required from me for this issue.
     @PostMapping("/note/{id}/delete")
-    @ApiOperation(
-            "Delete a note by ID as a POST request. You should be the owner of this note, otherwise, request is forbidden.")
     public ResponseEntity<ServiceResponse> postDeleteNoteById(
             @PathVariable("id") String id, Authentication auth) {
         Optional<Note> note = noteService.getRepository().findById(id);
@@ -104,7 +88,6 @@ public class NoteController {
     }
 
     @GetMapping("/notes")
-    @ApiOperation("Get all your notes with keywords match, if keyword query string is empty, return all")
     public List<Note> getNotes(Authentication auth, @RequestParam(required = false) String keyword) {
         JwtUser user = (JwtUser) auth.getPrincipal();
         List<Note> allNotes = noteService.getRepository().findAllByUserId(user.getId());
@@ -120,15 +103,14 @@ public class NoteController {
                         StringUtils.isNotBlank(n.getText()) && BaseUtils.containsAllIgnoreCase(n.getText(), queries))
                 .collect(Collectors.toList());
     }
+
     @PostMapping("/notes")
-    @ApiOperation("Get your notes with query conditions")
     public List<Note> getNotesByUrl(Authentication auth, @Valid @RequestBody NoteQuery noteQuery) {
         JwtUser user = (JwtUser) auth.getPrincipal();
         return noteService.getRepository().findAllByUserIdAndUrl(user.getId(), noteQuery.getUrl());
     }
 
     @PostMapping("/note")
-    @ApiOperation("Create a new note")
     public ResponseEntity<Note> createNote(@Valid @RequestBody Note note, Authentication auth) {
         JwtUser user = (JwtUser) auth.getPrincipal();
         note.setUserId(user.getId());
