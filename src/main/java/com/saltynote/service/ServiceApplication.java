d1c33755c84e80c158ab7a7c1045228afa2ba09f
Hao Zhou <hzhou.me@saltyee.com>
2023-03-31 22:54:02 -0700

    :recycle: add dedicated security config file

--- src/main/java/com/saltynote/service/ServiceApplication.java
+++ src/main/java/com/saltynote/service/ServiceApplication.java
@@ -5,18 +5,12 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
     @Bean
