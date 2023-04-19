e2d0ad3a4d6e64c59b7893a59aa6465f053609dc
Hao Zhou <hzhou.me@saltyee.com>
2023-04-19 11:16:10 -0700

    :recycle: replace lombok @Data for performance

--- src/main/java/com/saltynote/service/entity/SiteUser.java
+++ src/main/java/com/saltynote/service/entity/SiteUser.java
@@ -9,18 +9,27 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.Accessors;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.Objects;
 
 @Entity
 @Table(name = "user")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Accessors(chain = true)
 public class SiteUser implements Serializable, IdentifiableUser {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -50,6 +59,22 @@ public class SiteUser implements Serializable, IdentifiableUser {
     private void beforeSave() {
         this.id = FriendlyId.createFriendlyId();
         this.registerTime = new Timestamp(System.currentTimeMillis());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        SiteUser siteUser = (SiteUser) o;
+        return Objects.equals(id, siteUser.id) && Objects.equals(username, siteUser.username)
+                && Objects.equals(email, siteUser.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email);
     }
 
 }
