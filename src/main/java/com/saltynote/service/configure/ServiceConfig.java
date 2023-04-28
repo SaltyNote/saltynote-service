b828b5b2ea09539603aea7f9f1e992a91ff36b29
Hao Zhou <hzhou.me@saltyee.com>
2023-04-28 11:51:26 -0700

    :sparkles: Introduce SnowflakeIdGenerator

--- src/main/java/com/saltynote/service/configure/ServiceConfig.java
+++ src/main/java/com/saltynote/service/configure/ServiceConfig.java
@@ -1,8 +1,23 @@
 package com.saltynote.service.configure;
 
+import com.saltynote.service.generator.IdGenerator;
+import com.saltynote.service.generator.SnowflakeIdGenerator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServiceConfig {
+
+    @Value("${saltynote.datacenter.id}")
+    private int datacenterId;
+
+    @Value("${saltynote.machine.id}")
+    private int machineId;
+
+    @Bean
+    public IdGenerator snowflakeIdGenerator() {
+        return new SnowflakeIdGenerator(datacenterId, machineId);
+    }
 
 }
---
+++ src/main/java/com/saltynote/service/exception/ClockBackwardException.java
@@ -0,0 +1,9 @@
+package com.saltynote.service.exception;
+
+public class ClockBackwardException extends RuntimeException {
+
+    public ClockBackwardException(String message) {
+        super(message);
+    }
+
+}
